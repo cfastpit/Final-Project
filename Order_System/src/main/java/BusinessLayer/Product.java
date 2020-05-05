cb79d81745131cac63f650273f0b3dee52b77d64
theki <theki@DESKTOP-530HBLH>
2020-05-05 18:34:09 -0500

    Start of final

--- Order_System/src/main/java/BusinessLayer/Product.java
+++ Order_System/src/main/java/BusinessLayer/Product.java
@@ -5,4 +5,50 @@ public class Product {
   private String description;
   private int quantityOnHand;
   private double price;
+
+    public Product(int productID, String description, int quantityOnHand, double price) {
+        this.productID = productID;
+        this.description = description;
+        this.quantityOnHand = quantityOnHand;
+        this.price = price;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantityOnHand() {
+        return quantityOnHand;
+    }
+
+    public void setQuantityOnHand(int quantityOnHand) {
+        this.quantityOnHand = quantityOnHand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", description=" + description + ", quantityOnHand=" + quantityOnHand + ", price=" + price + '}';
+    }
+  
+  
 }
